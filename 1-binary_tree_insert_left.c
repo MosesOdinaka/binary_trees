0263564868084231655ddc5d9cbfa3ceb7beb97d
MosesOdinaka <iriele.moses@gmail.com>
2023-11-01 10:24:42 +0100

    Inserts a node as the right-child of another node.

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,33 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Add a node at the left of the parent, and
+ * if it exists it move down one level and add the new node first.
+ * @parent: parent of the key node.
+ * @value: value of the node.
+ * Return: NULL if it fails, or return the new node.
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (!parent)
+	{
+		return (NULL);
+	}
+
+	binary_tree_t *new_node = binary_tree_node(parent, value);
+
+	if (!new_node)
+	{
+		return (NULL);
+	}
+
+	if (parent->left)
+	{
+		new_node->left = parent->left;
+		new_node->left->parent = new_node;
+	}
+
+	parent->left = new_node;
+
+	return (new_node);
+}
